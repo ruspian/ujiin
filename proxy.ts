2f61be9cfc922c95abbcd585cee9cb0de4265062
ruspian <ruspianntb@gmail.com>
2026-05-20 09:03:44 +0800

    fix: ubah middleware ke proxy

--- proxy.ts
+++ proxy.ts
@@ -13,7 +13,7 @@ const rateLimitMap = new Map<string, RateLimitData>();
 const MAX_ATTEMPTS = 5;
 const TIME_WINDOW = 60 * 1000;
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const method = request.method;
 
